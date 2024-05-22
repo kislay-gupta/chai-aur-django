00a6a9883428e871083250971d1173195cff8bf7
kislay-gupta <kislaygupta007@gmail.com>
2024-05-23 01:24:23 +0530

    lesson 6 Handle model and urls in Django

--- chaiaurDjango/chai/models.py
+++ chaiaurDjango/chai/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+class ChaiVariety(models.Model):
+    CHAI_TYPE_CHOICE =[
+        ('ML','MASALA'),
+        ('GR','GINGER'),
+        ('KL','KIWI'),
+        ('PL','PLAIN'),
+        ('EL','ELACHI')
+    ]
+    name = models.CharField(max_length=100)
+    image= models.ImageField(upload_to="chai/")
+    date_added= models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2,choices=CHAI_TYPE_CHOICE)
+    description = models.TextField(default="")
+    price = models.CharField(max_length=2, default=0)
+
+    
+
+    def __str__(self):
+        return self.name
--- chaiaurDjango/chai/views.py
+++ chaiaurDjango/chai/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
-
+from .models import ChaiVariety
+from django.shortcuts import get_object_or_404
 def all_chai(request):
-    return render(request, 'all_chai.html')
+    chais = ChaiVariety.objects.all()
+    return render(request, 'chai/all_chai.html',{'chais':chais})
+
+def chai_details(request, chai_id):
+    chai = get_object_or_404(ChaiVariety, pk=chai_id)
+    return render(request,'chai/chai_details.html', {'chai':chai})
