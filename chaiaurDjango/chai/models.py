e2ee2490c498e9c5c86079475b0b73c26091afe0
kislay-gupta <kislaygupta007@gmail.com>
2024-05-30 21:53:04 +0530

    leason 7 relation in django

--- chaiaurDjango/chai/models.py
+++ chaiaurDjango/chai/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.core.exceptions import ValidationError
-
+from django.contrib.auth.models import User 
 # Create your models here.
 class ChaiVariety(models.Model):
     CHAI_TYPE_CHOICE =[
@@ -18,7 +18,41 @@ class ChaiVariety(models.Model):
     description = models.TextField(default="")
     price = models.CharField(max_length=2, default=0)
 
+    def __str__(self):
+        return self.name
     
+
+#one to many
+
+class ChaiReview(models.Model):
+    RATINGS = [(1, 1), (2,2), (3,3), (4,4), (5,5)]
+    chai = models.ForeignKey(ChaiVariety, on_delete=models.CASCADE, related_name='review')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=RATINGS)
+    comments = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+       return f'{self.user.username} review for {self.chai.name}'
+   
+# many to many
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location =  models.CharField(max_length=100)
+    chai_variety = models.ManyToManyField(ChaiVariety, related_name="stores")
 
     def __str__(self):
         return self.name
+    
+# one to one
+
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(ChaiVariety, on_delete=models.CASCADE, related_name="certificate")
+    certificate_number = models.CharField(max_length=100)
+    issue_date = models.DateTimeField(default=timezone.now)
+    valid_date = models.DateTimeField()
+
+    def __str__(self):
+     return f'Certificate for {self.chai.name}'
+    
